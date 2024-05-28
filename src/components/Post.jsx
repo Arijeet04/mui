90a735d31f7b94ce43ec9a67c74314f08eee6652
Arijeet Das Chowdhury <116735842+Arijeet04@users.noreply.github.com>
2024-05-28 22:43:32 +0530

    Change the Card Action in Post JSX

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -60,45 +60,6 @@ const Post = () => {
           <Share />
         </IconButton>
       </CardActions>
-        <CardHeader
-        avatar={
-          <Avatar 
-          src="https://e7.pngegg.com/pngimages/287/555/png-clipart-manchester-city-f-c-manchester-city-w-f-c-premier-league-2011-fa-cup-final-premier-league-text-trademark-thumbnail.png"
-          sx={{ bgcolor: "red",width: 45, height: 45 }} aria-label="recipe">
-            A
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVert />
-          </IconButton>
-        }
-        title="Arijeet Das Chowdhury"
-        subheader="July 7th, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="20%"
-        image="https://www.thesun.co.uk/wp-content/uploads/2016/07/nintchdbpict0002493605591-e1467837837720.jpg"
-        alt="Pep Guardiola"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-        Pep Guardiola had agreed to succeed him as manager, with his tenure beginning on 1 July 2016.
-        Despite a trophy-less first season in 2016–17, Guardiola would lead City to unprecedented success in the following six seasons.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
-        </IconButton>
-        <IconButton aria-label="add to favorites">
-          <Checkbox icon={<BookmarkBorder />} checkedIcon={<Bookmark />} />
-        </IconButton>
-        <IconButton aria-label="share">
-          <Share />
-        </IconButton>
-      </CardActions>
     </Card>
   );
 };
